1c0d2d2dc6870ef3386d54c78ba6c16c05259abc
Denise Stark <starkwalker88@gmail.com>
2024-08-06 13:12:08 -0700

    added place details

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,9 +1,18 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
-import { Colors } from '../../constants/colors';
-import PlaceItem from './PlaceItem';
+import { Colors } from "../../constants/colors";
+import PlaceItem from "./PlaceItem";
 
 function PlacesList({ places }) {
+  const navigation = useNavigation();
+
+  function selectPlaceHandler(id) {
+    navigation.navigate('PlaceDetails', {
+      placeId: id,
+    });
+  }
+  
   if (!places || places.length === 0) {
     return (
       <View style={styles.fallbackContainer}>
@@ -19,7 +28,9 @@ function PlacesList({ places }) {
       style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <PlaceItem place={item} />}
+      renderItem={({ item }) => (
+        <PlaceItem place={item} onSelect={selectPlaceHandler} />
+      )}
     />
   );
 }
@@ -32,8 +43,8 @@ const styles = StyleSheet.create({
   },
   fallbackContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   fallbackText: {
     fontSize: 16,
